dd41e7f870671ed64a4ff36bd08c4cee02dd8363
JagadishNaidu1994 <babblusrinivas1994@gmail.com>
2023-12-15 14:24:12 +0530

    testing variables

--- 6-datatypes.sh
+++ 6-datatypes.sh
@@ -7,3 +7,5 @@ NUMBER2=$2
 SUM=$(($NUMBER1+$NUMBER2))
 
 echo $SUM
+
+# dont worry abt data types
---
+++ 7-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+FRUITS=("Apple" "Banana" "Mango")
+
+echo "First fruit is ${FRUITS[0]}"
+
+
+echo "Second fruit is ${FRUITS[1]}"
+
+
+echo "Third fruit is ${FRUITS[2]}"
